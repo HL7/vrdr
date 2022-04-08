c59fdd773a2737d053843215b33c698b6ee2b6ed
Saul Kravitz <skravitz@mitre.org>
2022-04-08 13:27:31 -0400

    updated documentation

---
+++ input/pagecontent/StructureDefinition-vrdr-cause-of-death-coding-content-bundle-intro.md
@@ -0,0 +1,17 @@
+### Usage
+ Bundle containing  the following coded content:
+  * Coded Content
+    * [ActivityAtTimeOfDeath]
+    * [AutomatedUnderlyingCauseOfDeath]
+    * [ManualUnderlyingCauseOfDeath]
+    * [CodedRaceAndEthnicity]
+    * [EntityAxisCauseOfDeath]
+    * [RecordAxisCauseOfDeath]
+    * [PlaceOfInjury]
+    * [CodingStatusValues]
+
+
+| **#** |  **Description**   |  **IJE Name**   | IJE only |  **Field**  |  **Type**  | **Value Set**  |
+| :---------: | ------------- | ------------ | :----------: |---------- | -------- | -------- |
+{: .grid }
+{% include markdown-link-references.md %}
--- tools/makemarkdownfromexcel.rb
+++ tools/makemarkdownfromexcel.rb
@@ -236,8 +236,8 @@ profiles["CodingStatusValues"] = {
   :out => "StructureDefinition-vrdr-coding-status-values-intro.md",
   :desc => "",
 }
-profiles["CodedContentBundle"] = {
-  :out => "StructureDefinition-vrdr-coding-content-bundle-intro.md",
+profiles["CauseOfDeathCodedContentBundle"] = {
+  :out => "StructureDefinition-vrdr-cause-of-deathcoding-content-bundle-intro.md",
   :desc =>" Bundle containing  the following coded content:
   * Coded Content
     * [ActivityAtTimeOfDeath]
@@ -249,6 +249,31 @@ profiles["CodedContentBundle"] = {
     * [PlaceOfInjury]
     * [CodingStatusValues]
     ",
+  }
+  profiles["DemographicCodedContentBundle"] = {
+  :out => "StructureDefinition-vrdr-demographic-coding-content-bundle-intro.md",
+  :desc =>"
+  This bundle is information-content equivalent to the traditional NCHS TRX format.
+
+  This bundle contains:
+  * Coded Content:
+    * Activity at Time of Death [ActivityAtTimeOfDeath]
+    * Underlying Cause of Death -- [AutomatedUnderlyingCauseOfDeath] and [ManualUnderlyingCauseOfDeath]
+    * Entity Axis Cause of Death [EntityAxisCauseOfDeath]
+    * Record Axis Cause of Death [RecordAxisCauseOfDeath]
+    * Place of Injury [PlaceOfInjury]
+    * Coding Status Values [CodingStatusValues] - various flags that are part of the TRX format
+  * Input Content:
+    * Cause of Death Part 1 - [CauseOfDeathPart1] and [CauseOfDeathPathway]
+    * Cause of Death Part 2 - [CauseOfDeathPart2]
+    * Manner of Death - [MannerOfDeath]
+    * Autopsy Performed Indicator - [AutopsyPerformedIndicator]
+    * Death Certification - [DeathCertification]
+    * Injury Incident - [InjuryIncident]
+    * Tobacco Use Contributed to Death - [TobaccoUseContributedToDeath]
+    * Decedent Pregnancy Status - [DecedentPregnancyStatus]
+    * Surgery Date - [SurgeryDate]
+     ",
   }
 
 puts ARGV[0]
