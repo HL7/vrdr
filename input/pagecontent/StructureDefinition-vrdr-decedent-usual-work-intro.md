d8fce4b3397f01da75a1857bc5ec019f5a4d7288
Saul Kravitz <skravitz@mitre.org>
2022-06-13 11:13:17 -0400

    tweak documentation

--- input/pagecontent/StructureDefinition-vrdr-decedent-usual-work-intro.md
+++ input/pagecontent/StructureDefinition-vrdr-decedent-usual-work-intro.md
@@ -1,11 +1,12 @@
 ### Usage
 Implementors are free to use the coded fields with the defined valuesets for inter-jurisdictional exchange,
-            but coded values are not expected for death certificate submission. The underlying profile requires coded
-            values for validity, so a null flavor value can be used as in Example1. Similarly the effective period is required,
-            and null flavor values can be substituted.
+  but coded values are not expected for death certificate submission. The underlying profile requires coded
+  values for validity, so a null flavor value can be used as in Example1. Similarly the effective period is required,
+  and null flavor values can be substituted.  These constraints are expected to be relaxed in the ODH Usual Work profile prior
+  to VRDR publication.
 
-            Note that the coded industry and occupation fields are bound to PHINVADs-hosted value sets.  These are the only PHINVADs
-            references in this IG.  For NCHS provided coded values the specific vocabulary used will depend on the death year.
+  Note that the coded industry and occupation fields are bound to PHINVADs-hosted value sets.  These are the only PHINVADs
+  references in this IG.  For NCHS provided coded values the specific vocabulary used will depend on the death year.
 
 | **#** |  **Description**   |  **IJE Name**   | IJE only |  **Field**  |  **Type**  | **Value Set**  |
 | :---------: | ------------- | ------------ | :----------: |---------- | -------- | -------- |
--- tools/makemarkdownfromexcel.rb
+++ tools/makemarkdownfromexcel.rb
@@ -168,12 +168,13 @@ profiles["DecedentPregnancyStatus"] = {
 profiles["DecedentUsualWork"] = {
   :out => "StructureDefinition-vrdr-decedent-usual-work-intro.md",
   :desc => "Implementors are free to use the coded fields with the defined valuesets for inter-jurisdictional exchange,
-            but coded values are not expected for death certificate submission. The underlying profile requires coded
-            values for validity, so a null flavor value can be used as in Example1. Similarly the effective period is required,
-            and null flavor values can be substituted.
+  but coded values are not expected for death certificate submission. The underlying profile requires coded
+  values for validity, so a null flavor value can be used as in Example1. Similarly the effective period is required,
+  and null flavor values can be substituted.  These constraints are expected to be relaxed in the ODH Usual Work profile prior
+  to VRDR publication.
 
-            Note that the coded industry and occupation fields are bound to PHINVADs-hosted value sets.  These are the only PHINVADs
-            references in this IG.  For NCHS provided coded values the specific vocabulary used will depend on the death year.",
+  Note that the coded industry and occupation fields are bound to PHINVADs-hosted value sets.  These are the only PHINVADs
+  references in this IG.  For NCHS provided coded values the specific vocabulary used will depend on the death year.",
 }
 profiles["DispositionLocation"] = {
   :out => "StructureDefinition-vrdr-disposition-location-intro.md",
