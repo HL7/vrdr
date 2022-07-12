eca5bedf4f3420d938a8bb090c4200ba9f445c8c
Saul Kravitz <skravitz@mitre.org>
2022-07-12 09:46:33 -0400

    updated documentation

--- input/pagecontent/StructureDefinition-vrdr-decedent-age-intro.md
+++ input/pagecontent/StructureDefinition-vrdr-decedent-age-intro.md
@@ -1,5 +1,18 @@
 ### Usage
+The DecedentAge profile of Observation records the decedents age at time of death.
 
+  Notes:
+  * When present, the effective date of this observation shall be equal to the decedent's date of death.
+  * Age is derived as the difference in time between the decedent's death date and birth date.
+  * The appropriate units for age are defined here:
+
+  |  Age Range  | Units |
+  |:---:|:---:|
+  | age < 1 hr | minutes |
+  | 1 hr &le; age < 24 hours | hours |
+  | 1 day &le; age < 28 days (1 "month") | days |
+  | 1 month &le;  age  < 12 months | months |
+  | 1 year &le; age | years|
 
 | **#** |  **Description**   |  **IJE Name**   | IJE only |  **Field**  |  **Type**  | **Value Set**  |
 | :---------: | ------------- | ------------ | :----------: |---------- | -------- | -------- |
--- tools/makemarkdownfromexcel.rb
+++ tools/makemarkdownfromexcel.rb
@@ -150,7 +150,20 @@ profiles["Decedent"] = { :out => "StructureDefinition-vrdr-decedent-intro.md",
   due to lack of a value for gender." }
   profiles["DecedentAge"] = {
   :out => "StructureDefinition-vrdr-decedent-age-intro.md",
-  :desc => "",
+  :desc => "The DecedentAge profile of Observation records the decedents age at time of death.
+
+  Notes:
+  * When present, the effective date of this observation shall be equal to the decedent's date of death.
+  * Age is derived as the difference in time between the decedent's death date and birth date.
+  * The appropriate units for age are defined here:
+
+  |  Age Range  | Units |
+  |:---:|:---:|
+  | age < 1 hr | minutes |
+  | 1 hr &le; age < 24 hours | hours |
+  | 1 day &le; age < 28 days (1 \"month\") | days |
+  | 1 month &le;  age  < 12 months | months |
+  | 1 year &le; age | years|",
 }
 profiles["DecedentDispositionMethod"] = {
   :out => "StructureDefinition-vrdr-decedent-disposition-method-intro.md",
