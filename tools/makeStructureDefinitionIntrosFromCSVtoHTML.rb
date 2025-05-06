89f9e0e9b5b4a526f4eb2e8b96f9e03f8eb09476
Saul Kravitz <skravitz@mitre.org>
2025-05-06 17:53:13 -0400

    use same skip as in other repos

--- tools/makeStructureDefinitionIntrosFromCSVtoHTML.rb
+++ tools/makeStructureDefinitionIntrosFromCSVtoHTML.rb
@@ -97,7 +97,7 @@ vFormsMappingSpreadsheet = ARGV[2]
 def gen_aliases
   aliases={}
   File.foreach(Dir.pwd + "/input/includes/markdown-link-references.md", chomp: true) do |line|
-      next unless line.start_with?("[")
+      next if line.start_with?('<!-', '{%')|| line.strip.empty?
       parts = line.split(':',2)
       url =parts[1][1..] if !parts[1].nil?
       link = parts[0][1..-2]
